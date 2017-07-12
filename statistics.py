43252c87810b0961134552de0d1382ecca60b193
Adam Budai <budai8adam@gmail.com>
2017-07-12 16:14:27 +0200

    The loss in statistics.py is also handled.

--- statistics.py
+++ statistics.py
@@ -24,16 +24,16 @@ def collect_reward_file_names():
         with open(file_name, "r") as f:
             rewards = json.load(f)
         
-        sum = 0.0
+        summa = 0.0
         for x in rewards:
-            sum += x
+            summa += x
             
-        if learner_id not in total_rewards:
+        if learner_id not in list(total_rewards):
             total_rewards[learner_id] = {}
-        if iteration in total_rewards[learner_id]:
-             total_rewards[learner_id][iteration].append(sum)
+        if iteration in list(total_rewards[learner_id]):
+             total_rewards[learner_id][iteration].append(summa)
         else:
-            total_rewards[learner_id] = { iteration: [sum] }
+            total_rewards[learner_id][iteration] = [summa]
     
     return total_rewards
     
@@ -80,11 +80,41 @@ def calculate_min_rewards(total_rewards, learner_id):
                 min_rewards[it] = rw
         
     return [min_rewards, iterations]
+
+def collect_loss_file_names():
+    total_losses = {} 
+    list_of_names = os.listdir(path_losses)
     
-def draw_simple_graphs(x, y, y_label):
+    for idx in range(0, len(list_of_names)):
+        attributes = list_of_names[idx].split(".")[0] # Cut the json part from the string.
+        attributes = attributes.split("/") # Cut the string part with the numbers.
+        attributes = attributes[0].split("_")[1:3] # Contains the iteration, learner_id.
+        
+        iteration = int(attributes[0])
+        learner_id = int(attributes[1])
+        
+        file_name = path_losses + list_of_names[idx]
+        with open(file_name, "r") as f:
+            loss = json.load(f)
+            
+        if learner_id not in list(total_losses):
+            total_losses[learner_id] = {}
+        if iteration not in list(total_losses[learner_id]):
+             total_losses[learner_id][iteration] = loss
     
-    plt.figure(1)
-    plt.plot(x, y)
+    return total_losses
+
+def loss_of_learner(total_losses, learner_id):
+    
+    iterations = list(total_losses[learner_id])
+    losses = list(total_losses[learner_id].values())
+    
+    return [losses, iterations]
+    
+def draw_simple_graphs(num, x, y, y_label):
+    
+    plt.figure(num)
+    plt.plot(x, y, 'ro')
     plt.xlabel('Number of actions')
     plt.ylabel(y_label)
     plt.show()
