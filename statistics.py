d73c26a1674a3acb22a39d4da77e11480bd416e6
Adam Budai <budai8adam@gmail.com>
2017-07-18 09:12:09 +0200

    Statistics was refactored.

--- statistics.py
+++ statistics.py
@@ -2,6 +2,7 @@ import json
 import os
 import matplotlib.pyplot as plt
 import argparse
+import math as m
 
 parser = argparse.ArgumentParser(description='A3C statistics')
 
@@ -11,14 +12,26 @@ parser.add_argument('--show-all', action='store_true',
 
 path_rewards = 'files/rewards/'
 path_losses = 'files/losses/'
+path_meta = 'files/metadata.json'
 
 # CREATE PLOTS and draw some STATISTICAL DATA
+
+def step_progress_bar():
+    print ('* ', end="")
 
 def collect_reward_file_names():
     total_rewards = {} 
     list_of_names = os.listdir(path_rewards)
+    prev_progress = 0
+    progress = 0
     
-    for idx in range(0, len(list_of_names)):
+    print ('Processing reward files.')
+    for idx in range(len(list_of_names)):
+        progress = m.floor(float(idx)/float(len(list_of_names)) * 10)
+        if progress > prev_progress:
+            step_progress_bar()
+            prev_progress = progress
+        
         attributes = list_of_names[idx].split(".")[0] # Cut the json part from the string.
         attributes = attributes.split("/") # Cut the string part with the numbers.
         attributes = attributes[0].split("_")[1:4] # Contains the iteration, learner_id and rnd values.
@@ -41,57 +54,48 @@ def collect_reward_file_names():
         else:
             total_rewards[learner_id][iteration] = [summa]
     
+    print (' -> DONE')
+    
     return total_rewards
     
-def calculate_avg_rewards(total_rewards, learner_id):
-    
+def calculate_reward_statistics(total_rewards, learner_id):
     iterations = list(total_rewards[learner_id])
     
     # Calculating the averages.
     avg_rewards = [0] * len(iterations)
     
-    for it in range(len(iterations)):
-        for rw in total_rewards[learner_id][iterations[it]]:
-            avg_rewards[it] += rw
-        avg_rewards[it] = avg_rewards[it]/len(total_rewards[learner_id][it])
-        
-    return [avg_rewards, iterations]
-    
-def calculate_max_rewards(total_rewards, learner_id):
-    
-    iterations = list(total_rewards[learner_id])
-    
     # Calculating the averages.
     max_rewards = [0] * len(iterations)
-    
-    for it in range(len(iterations)):
-        max_rewards[it] = total_rewards[learner_id][iterations[it]][0]
-        for rw in total_rewards[learner_id][iterations[it]]:
-            if max_rewards[it] < rw:
-                max_rewards[it] = rw
-        
-    return [max_rewards, iterations]
-    
-def calculate_min_rewards(total_rewards, learner_id):
-    
-    iterations = list(total_rewards[learner_id])
     
     # Calculating the averages.
     min_rewards = [0] * len(iterations)
     
     for it in range(len(iterations)):
         min_rewards[it] = total_rewards[learner_id][iterations[it]][0]
+        max_rewards[it] = total_rewards[learner_id][iterations[it]][0]
         for rw in total_rewards[learner_id][iterations[it]]:
+            avg_rewards[it] += rw           
             if min_rewards[it] > rw:
                 min_rewards[it] = rw
+            if max_rewards[it] < rw:
+                max_rewards[it] = rw
+        avg_rewards[it] = avg_rewards[it]/len(total_rewards[learner_id][iterations[it]])
         
-    return [min_rewards, iterations]
+    return [avg_rewards, min_rewards, max_rewards, iterations]
 
 def collect_loss_file_names():
     total_losses = {} 
     list_of_names = os.listdir(path_losses)
+    prev_progress = 0
+    progress = 0
     
-    for idx in range(0, len(list_of_names)):
+    print ('Processing loss files.') 
+    for idx in range(len(list_of_names)):
+        progress = m.floor(float(idx)/float(len(list_of_names)) * 10)
+        if progress > prev_progress:
+            step_progress_bar()
+            prev_progress = progress
+        
         attributes = list_of_names[idx].split(".")[0] # Cut the json part from the string.
         attributes = attributes.split("/") # Cut the string part with the numbers.
         attributes = attributes[0].split("_")[1:3] # Contains the iteration, learner_id.
@@ -107,6 +111,8 @@ def collect_loss_file_names():
             total_losses[learner_id] = {}
         if iteration not in list(total_losses[learner_id]):
              total_losses[learner_id][iteration] = loss
+    
+    print (' -> DONE')
     
     return total_losses
 
@@ -126,18 +132,23 @@ def draw_simple_graphs(num, x, y, y_label):
     plt.show()
 
 def show_all():
+    
+    with open(path_meta, 'r') as f:
+        metadat = json.load(f)
+    print (metadat)
+    
     tot_rwds = collect_reward_file_names()
     tot_losses = collect_loss_file_names()
     
-    min_data = calculate_min_rewards(tot_rwds, 0)
-    max_data = calculate_max_rewards(tot_rwds, 0)
-    avg_data = calculate_avg_rewards(tot_rwds, 0)
+    print('Calculate statistics.')
+    reward_stats = calculate_reward_statistics(tot_rwds, 0)
     loss_data = loss_of_learner(tot_losses, 0)
-
-    draw_simple_graphs(1, min_data[1], min_data[0], 'minimum reward')
-    draw_simple_graphs(2, max_data[1], max_data[0], 'maximum reward')
-    draw_simple_graphs(3, avg_data[1], avg_data[0], 'average reward')
-    draw_simple_graphs(4, loss_data[1], avg_data[0], 'loss')
+    
+    print('Show graphs.')
+    draw_simple_graphs(1, reward_stats[3], reward_stats[0], 'average reward')
+    draw_simple_graphs(2, reward_stats[3], reward_stats[1], 'minimum reward')
+    draw_simple_graphs(3, reward_stats[3], reward_stats[2], 'maximum reward')
+    draw_simple_graphs(4, loss_data[1], loss_data[0], 'loss')
 
 if show_all:
     show_all()
